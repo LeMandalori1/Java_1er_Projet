55a28642b558eacbe02e9b67b5e1a9e465b4495a
LeMandalori1 <gael.hericher@gmail.com>
2024-06-19 23:16:21 +0200

    Petite sauvegarde

--- Deuxieme.java
+++ Deuxieme.java
@@ -8,7 +8,17 @@ public class Deuxieme {
     
     public static void main(String[] args){
 
-        System.out.println("Hello world !!");
+        String exemple = "Hello";
+
+        //String est une classe, qui possède donc de nombreuses méthodes comme "toUpperCase" qui met tout en majuscule
+
+        exemple = exemple.toUpperCase();
+
+        //La méthode replace remplace une partie de notre texte par une autre (on premier on lui dit quelle partie remplacer, puis avec quoi)
+
+        exemple = exemple.replace("LO", "LOLO");
+
+        System.out.println(exemple + " world !!");
         
     }
 }
