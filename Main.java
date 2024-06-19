05cfed4b3bb84464115cdff9eb9bf544b9a9bb24
LeMandalori1 <gael.hericher@gmail.com>
2024-06-19 22:26:02 +0200

    Allez ça va bien finir par fonctionner

---
+++ Main.java
@@ -0,0 +1,13 @@
+package Java_1er_Projet;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int incomes = 500;
+
+        System.out.println(incomes);
+
+    } 
+    
+}
