f11654357931281a2c8cf631ec90b29a9297add6
LeMandalori1 <gael.hericher@gmail.com>
2024-06-19 22:57:22 +0200

    1ere version du style d'un cours de base

--- Main.java
+++ Main.java
@@ -1,13 +1,51 @@
 package Java_1er_Projet;
 
 public class Main {
-
+   
     public static void main(String[] args) {
 
-        int incomes = 500;
+        //Pour une constante, on met "final" devant
 
-        System.out.println(incomes);
+        final String meilleurPays = "France";
 
-    } 
-    
-}
+        //Pour les nombres à virgules, "float" est bien mais deux fois moins précis que "double" 
+
+        double nombre2 = 123.123456;
+
+        //Si je dois faire des divisions, je dois utiliser absolument des duoble et pas des int
+
+        float a = 2;
+
+        float b = 5;
+
+        float c = a/b;
+
+        System.out.println(c);
+
+        //Le booléen (True ou False)
+
+        boolean imVeryGoodInJava = false;
+
+        boolean noImJustJoking = true;
+
+        imVeryGoodInJava = true;
+
+        //Les Strings
+
+        String stringVide = "";
+
+        final int nombreDeCouleursDrapeauFrançais = 3;
+       
+        String text1 = "Vive la France !!";
+
+        String text2 = text1 + " Ouais. \n" + nombreDeCouleursDrapeauFrançais + " couleurs et c'est tout.";
+
+        System.out.println(text2);
+
+        int numberOfVoyelles = 0;
+
+        double pourcentageOfVoyelles = 0.0;
+
+       //Premier commentaire de ma vie en Java
+    }
+ }
